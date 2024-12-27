140144d3584a0ef7fd064ad9e7aa21ea8ff6cb30
eniszgr <zgr.enis@gmail.com>
2024-12-27 10:43:40 +0300

    link

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekanbul-26aralik-oontz9dmc-enis-projects-4c5edf93.vercel.app//api",
+  baseURL: "https://mekanbul-26aralik.vercel.app/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
